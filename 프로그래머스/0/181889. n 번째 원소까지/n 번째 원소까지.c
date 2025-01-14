fbee1b890d76ddd94b60e41d6edf08f9f6df65b2
Jingood <155884785+Jingood@users.noreply.github.com>
2025-01-14 21:15:33 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int* solution(int num_list[], size_t num_list_len, int n) {
+    int* answer = (int*)malloc(n * sizeof(int));
+    for(int i=0 ; i<n ; i++)
+        answer[i] = num_list[i];
+    return answer;
+}
