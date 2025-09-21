b14876dfefeaaaa4cf24719d91536b28f44c9df4
Jingood <155884785+Jingood@users.noreply.github.com>
2025-09-21 19:06:59 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.05 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double n1 = num1;
+        double n2 = num2;
+        int answer = (int)((n1 / n2) * 1000);
+        return answer;
+    }
+}
