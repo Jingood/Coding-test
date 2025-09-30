d76cef79aede990ebc27aaab69c73998325bfe90
Jingood <155884785+Jingood@users.noreply.github.com>
2025-09-30 21:56:19 +0900

    [level 0] Title: 편지, Time: 0.05 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        StringBuilder sb = new StringBuilder(message);
+        int len = sb.length();
+        int answer = 2 * len;
+        return answer;
+    }
+}
